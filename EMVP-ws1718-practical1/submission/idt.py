aba4ea906ba6f0bcbd204d06d6a29698164ba65e
zxmxt11 <vanessa.kirchner@student.uni-tuebingen.de>
2017-11-16 20:24:10 +0100

    01_2.1 read and show data file

--- EMVP-ws1718-practical1/submission/idt.py
+++ EMVP-ws1718-practical1/submission/idt.py
@@ -1,1 +1,15 @@
-Your code here.
+#! python
+import sys
+import csv
+
+# Test Python version
+# sys.stdout.write("hello from Python %s\n" % (sys.version,))
+
+# Read data
+with open('data.csv', 'rt', encoding='utf-8') as csvfile:
+	spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+	
+	# Show data
+	for row in spamreader:
+		string = ", ".join(row)
+		print(string)
