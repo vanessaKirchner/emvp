151715c10c9d96d3a0ea8e16b0d3fc3819161d3a
zxmxt11 <vanessa.kirchner@student.uni-tuebingen.de>
2017-11-20 16:02:37 +0100

    01_1 change tab format and semicolon

--- EMVP-ws1718-practical1/submission/ivt.py
+++ EMVP-ws1718-practical1/submission/ivt.py
@@ -2,111 +2,98 @@
 import sys
 import csv
 import numpy
-import matplotlib.pyplot as plt;
-
+import matplotlib.pyplot as plt
 
 
 ############ Exercise 2.2
 
-def removeInvalid(data):	
-	# Remove invalid data points
+def removeInvalid(data):
+    # Remove invalid data points
 
-	erased = []
+    erased = []
 
-	for x in range(2,len(data)) :
+    for x in range(2, len(data)):
 
-		if (data[x][5] != '0') and (data[x][10] != '0'):
-			erased.append(data[x])
+        if (data[x][5] != '0') and (data[x][10] != '0'):
+            erased.append(data[x])
 
-	return erased
+    return erased
 
 
 def plotPoints(image_name, data):
-	image = plt.imread(image_name);
-	implot = plt.imshow(image);
+    image = plt.imread(image_name)
+    implot = plt.imshow(image)
 
-	for x in range(2,len(data)) :
+    for x in range(2, len(data)):
+        # left eye
+        plt.scatter([data[x][1]], [data[x][2]], c='b')
 
-		# left eye
-		plt.scatter([data[x][1]], [data[x][2]], c='b')
+        # right eye
+        plt.scatter([data[x][6]], [data[x][7]], c='r')
 
-		# right eye
-		plt.scatter([data[x][6]], [data[x][7]], c='r')
-
-
-	plt.savefig('eyes-overlay.png')
-	plt.show()
+    plt.savefig('eyes-overlay.png')
+    plt.show()
 
 
 ############ Exercise 3 I-VT
 
 def ivt(data, velocity_threshold):
-	fixation_threshold = 100  #TODO benutzen?
-	saccade_threshold = 300  # TODO benutzen?
+    fixation_threshold = 100  # TODO benutzen?
+    saccade_threshold = 300  # TODO benutzen?
 
+    fixations = []
 
-	fixations = []
+    for point in range(0, len(data) - 1):
 
-	for point in range(0, len(data)-1):
+        # Calculate the point-to-point velocities for each point in the protocol
+        velocity = float(data[point + 1][0]) - float(data[point][0])
 
-		#Calculate the point-to-point velocities for each point in the protocol
-		velocity = float(data[point + 1][0]) - float(data[point][0])
+        # Label each point below velocity threshold as a fixation point, otherwise saccade
+        if velocity < velocity_threshold:
+            data[point].append('1')  # 1 is fixation, 0 is saccade
+        else:
+            data[point].append('0')
 
-		# Label each point below velocity threshold as a fixation point, otherwise saccade
-		if velocity < velocity_threshold:		
-			data[point].append('1')	 # 1 is fixation, 0 is saccade
-		else:
-			data[point].append('0')
+    # Collapse consecutive fixation points into fixation groups, removing saccade points
+    for point in range(0, len(data) - 1):
+        if (data[point][11] != '0'):
+            fixations.append(data[point])
 
+    # map each fixation group to a fixation at the centroid of its points
 
-	# Collapse consecutive fixation points into fixation groups, removing saccade points
-	for point in range(0, len(data)-1):
-		if (data[point][11] != '0'):
-			fixations.append(data[point])
+    # TODO
 
-
-	# map each fixation group to a fixation at the centroid of its points
-
-	# TODO
-
-	# Return fixations
-	return fixations
-
-
+    # Return fixations
+    return fixations
 
 
 def main():
+    ############ Exercise 2.1
 
-	############ Exercise 2.1
+    # Read data
+    datalist = []
 
-	# Read data
-	datalist = []
+    # Read data and save to list
+    with open('data.csv', 'rt', encoding='utf-8') as csvfile:
+        data = csv.reader(csvfile, delimiter='\t', quotechar='|')
+        datalist = list(data)
 
-	# Read data and save to list
-	with open('data.csv', 'rt', encoding='utf-8') as csvfile:
-		data = csv.reader(csvfile, delimiter='\t', quotechar='|')
-		datalist = list(data)
+    ############ Exercise 2.2
 
-
-	############ Exercise 2.2
-
-	# Remove invalid data points
-	erased = removeInvalid(datalist)
-	# TODO: Sind das wirklich alle die Invalid sind?
+    # Remove invalid data points
+    erased = removeInvalid(datalist)
+    # TODO: Sind das wirklich alle die Invalid sind?
 
 
-	# Show data points on image
-	image_name = 'stimuli.jpg'	
-	#plotPoints(image_name, datalist)
+    # Show data points on image
+    image_name = 'stimuli.jpg'
+    # plotPoints(image_name, datalist)
 
 
 
-	# Exercise 3 - IVT
-	velocity_threshold = 8250  # TODO Macht das Sinn? Irgendwie kommen da nur so große abstände raus
-	fixations_ivt = ivt(erased, velocity_threshold)
+    # Exercise 3 - IVT
+    velocity_threshold = 8250  # TODO Macht das Sinn? Irgendwie kommen da nur so große abstände raus
+    fixations_ivt = ivt(erased, velocity_threshold)
 
 
 if __name__ == "__main__": main()
-
-
-
